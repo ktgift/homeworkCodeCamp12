d4c1f58942a4ede957f4cf2b87cfea4e33bd4c82
ktgift <kavisara.suk@gmail.com>
2022-04-26 14:28:19 +0700

    add lap3.1 - 3.5

---
+++ javascript/variablesLap1.js
@@ -0,0 +1,31 @@
+// Lap3.1
+const name = 'Kavisara';
+let person=(name); //ตั้งว่า name ได้มันเป็น เตือนของ VScode เฉยๆ
+alert(person);
+
+// Lap3.2
+let money;
+const myParents = 'somkag and lawan';
+const myAddress = 'ladkrabung';
+let universeAge = 30000;
+
+// Lap3.3
+const user = 'iamhero';
+const role = 'customer';
+// alert(username); It is error!!
+alert(user + name);
+
+// Lab3.4
+const firstname ='Kavisara';
+let lastname = 'Thisakobsuk';
+var nickname = 'Gift';
+
+// IT is error --> Uncaught SyntaxError: Identifier 'firstname' has already been declared ห้ามใช้ซ้ำ
+// const firstname ='Suriya';
+// let lastname = 'Tungsirin';
+
+var nickname = 'Jack';
+brithday = '28 Nov 1991';
+
+// Lap3.5
+const BRAND_NAME = 'I am a hero';
