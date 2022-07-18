695cf441b8afc6dbfd98566ba57bcc10b4b1ff78
ktgift <kavisara.suk@gmail.com>
2022-07-18 21:04:02 +0700

    finish lab4-1

---
+++ react/my-lab/my-lab/src/App.js
@@ -0,0 +1,15 @@
+function Hello() {
+  return (
+    <div>
+      <h1>Hello My Friend</h1>
+      <p>Friend List</p>
+      <ul>
+        <li>John</li>
+        <li>Jack</li>
+        <li>Jane</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Hello;
