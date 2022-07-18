c331eed0838ffa0d9ed8329144b6633f3409ad40
ktgift <kavisara.suk@gmail.com>
2022-07-18 21:44:57 +0700

    finish lab4-4

--- react/my-lab/my-lab/src/App.js
+++ react/my-lab/my-lab/src/App.js
@@ -1,11 +1,14 @@
-
 function App() {
   return (
-    <div style={{margin: "20px"}}>
-     <button>+</button>
-     <span>0</span>
-     <button>-</button>
-     <button type='reset'>Reset</button>
+    <div>
+      <button>Add</button>
+      <br />
+      <del>Task 1</del>
+      <button>Edit</button>
+      <button>Del</button><br />
+      <span>Task 2</span>
+      <button>Edit</button>
+      <button>Del</button>
     </div>
   );
 }
