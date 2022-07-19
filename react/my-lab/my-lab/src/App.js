828a5f3a91adb793ea1bc01b665b5650c286ae8c
ktgift <kavisara.suk@gmail.com>
2022-07-19 15:51:02 +0700

    finish lab5-2

--- react/my-lab/my-lab/src/App.js
+++ react/my-lab/my-lab/src/App.js
@@ -1,21 +1,36 @@
-
 function App() {
   return (
-    // inline 
-    // <div style={{ textAlign: 'center', marginTop: '30rem' }}>
-    //   <button>+</button>
-    //   <span style={{ marginLeft: 10, marginRight: 10 }}>0</span>
-    //   <button style={{ marginRight: 10 }}>-</button>
-    //   <button>Reset</button>
-    // </div>
-
-    // external
-    <div className="div">
-      <button className="buttoms">+</button>
-      <span className="buttoms">0</span>
-      <button className="buttoms">-</button>
-      <button>Reset</button>
+    <>
+    {/* inline  */}
+    <div>
+        <button style={{ display: "block" }}>Add</button>
+        <div style={{ display: "block" }}>
+          <del style={{ marginTop: '10px', marginRight: '10px', color: 'green' }}>Task 1</del>
+          <button style={{ marginTop: '10px', marginRight: '10px' }}>Edit</button>
+          <button>Del</button>
+        </div>
+        <div style={{ display: "block" }}>
+          <span style={{ marginTop: '10px', marginRight: '10px' }}>Task 2</span>
+          <button style={{ marginTop: '10px', marginRight: '10px' }}>Edit</button>
+          <button>Del</button>
+        </div>
     </div>
+
+    {/* external */}
+    <div>
+        <button>Add</button>
+        <div classNamr="div">
+          <span className="buttoms text">Task 1</span>
+          <button className="buttoms">Edit</button>
+          <button>Del</button>
+        </div>
+        <div classNamr="div">
+          <span className="buttoms">Task 2</span>
+          <button className="buttoms">Edit</button>
+          <button>Del</button>
+        </div>
+    </div>
+  </>
   );
 }
 
