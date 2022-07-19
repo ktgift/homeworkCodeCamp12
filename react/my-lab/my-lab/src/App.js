7f924c347fc050770d3fb027fded3d50d1446324
ktgift <kavisara.suk@gmail.com>
2022-07-19 18:57:59 +0700

    finish lab6-1

--- react/my-lab/my-lab/src/App.js
+++ react/my-lab/my-lab/src/App.js
@@ -1,38 +1,11 @@
 function App() {
   return (
-    <>
-      <div className="titleBar center">
-          <button className="buttonStart">New Game</button>
-      </div>
-      <div className="flex-container">
-        <div className="pink">
-          <h2>PLAYER 1</h2>
-          <p style={{ fontSize: '40px', marginTop: '25%' }}>0</p>
-        </div>
-        <div className="border">
-          <div>
-            <button className="roll">Roll</button>
-            <button className="keep">Keep</button>
-          </div>
-
-          <div className="container">
-            <div className="div border box">
-              <p>1</p>
-            </div>
-          </div>
-          <p style={{ marginTop: '45%' }}>Round Score : 0</p>
-        </div>
-
-        <div className="border">
-          <h2>PLAYER 2</h2>
-          <p style={{ fontSize: '40px', marginTop: '25%' }}>0</p>
-        </div>
-      </div>
-      
-
-    </>
+   <input 
+    className="position"
+    placeholder="Enter your message"  
+    onChange={(event) => console.log(event.target.value)} 
+  />
   );
 }
-
 
 export default App;
