589db9a2e8b65323e988aa989cca2ea1853f36d9
ktgift <kavisara.suk@gmail.com>
2022-07-19 19:49:38 +0700

    change branch

--- react/my-lab/my-lab/src/App.js
+++ react/my-lab/my-lab/src/App.js
@@ -1,17 +1,8 @@
 function App() {
-  const handleClick = (event) => {
-    event.preventDefault();
-    const response = window.confirm('Leave for Google ?');
-    if(response) {
-      window.location.assign('https://google.com')
-    }
-  }
 
   return (
-    <div style={{ margit: '30px', fontSize: 40 }}>
-      <a href="https://google.com" onClick={handleClick}>
-        Google
-      </a>
+    <div style={{ margin: '30px' }}>
+    
     </div>
 
   )
