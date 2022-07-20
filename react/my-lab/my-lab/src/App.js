22ae0ccd17e67f886928615539d2e6ce1aaab2ef
ktgift <kavisara.suk@gmail.com>
2022-07-20 09:09:45 +0700

    finish lab7-1

--- react/my-lab/my-lab/src/App.js
+++ react/my-lab/my-lab/src/App.js
@@ -1,8 +1,14 @@
+import { useState } from 'react'
+
 function App() {
+  const [hideBtn, setBtnHide] = useState('');
 
   return (
     <div style={{ margin: '30px' }}>
-    
+      <button onClick={(event) => {
+        setBtnHide(event.target.hidden = true)
+      }}>
+      Click to hide me</button>
     </div>
 
   )
