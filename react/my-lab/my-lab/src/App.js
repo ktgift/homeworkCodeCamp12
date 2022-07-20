7b91eb41db962868e20a8313f64796570c5d279e
ktgift <kavisara.suk@gmail.com>
2022-07-20 10:26:43 +0700

    finish lab7-5

--- react/my-lab/my-lab/src/App.js
+++ react/my-lab/my-lab/src/App.js
@@ -1,14 +1,38 @@
 import { useState } from 'react'
 
 function App() {
-  const [state, setState] = useState(0);
+  const [number, setNumber] = useState('');
+  const [showError, setShowError] = useState('');
+
+  const handleNumberChange = event => {
+    setNumber(event.target.value);
+  }
+
+  const handleClickCheck = () => {
+    if (number === '') {
+      setShowError('Phone Number is required')
+    } else if (number.length !== 10) {
+      setShowError('Invalid length')
+    } else if (number.match("^[0-9]*$")) {
+      setShowError('')
+      alert('Your number is valid')
+    } else {
+      setShowError('Phone Number is invalid')
+    }
+  }
 
   return (
     <div style={{ margin: '30px' }}>
-      <label htmlFor='inputNumber'>Enter Dollar: </label>
-      <input id="inputNumber" onChange={(event) => setState(event.target.value * 30)} />
+      <input 
+        id="inputNumber" 
+        placeholder="Phone Number"
+        className={showError === '' ? '' : 'error'}
+        value={number}
+        onChange={handleNumberChange} />
+      
+      <button onClick={handleClickCheck}>Check</button>
 
-      <p>Convert to Bath: {state}</p>
+      {showError === '' ? null : <p className='error'>{showError}</p>}
     </div>
 
   )
