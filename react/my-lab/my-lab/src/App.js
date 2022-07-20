c307ab6fce1eb7549d026deb1ac2723a6b27708f
ktgift <kavisara.suk@gmail.com>
2022-07-20 16:02:40 +0700

    finish lab8-3

--- react/my-lab/my-lab/src/App.js
+++ react/my-lab/my-lab/src/App.js
@@ -3,28 +3,23 @@ import { useState } from 'react'
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, message: 'Good', date: '12-12-2022', sender: 'Jack'},
-    {id: 2, message: 'Very Good', date: '05-05-2022', sender: 'M'},
-    {id: 3, message: 'Great', date: '12-02-2022', sender: 'Koy'},
-    {id: 4, message: 'gooooooood', date: '07-12-2022', sender: 'Ground'},
-    {id: 5, message: 'Happy', date: '01-12-2022', sender: 'Gift'},
+    { id: '1', message: 'Happy new year', date: '01-06-2022 00.01 AM', author: 'John'},
+    { id: '2', message: 'Programing', date: '01-07-2022 10.23AM', author: 'Jack'},
+    { id: '3', message: 'Oh Amazing !!!', date: '01-19-2022 08.17PM', author: 'Sarah'}
   ]);
 
   const lists = posts.map((el) => 
-    <li key={el.id}>
-      <span style={{color: 'red'}}>Comment: </span>{el.message + " "} 
-      <span style={{color: 'red'}}>Date: </span>{el.date + " "}
-      <span style={{color: 'red'}}>Sender: </span>{el.sender + " "}
-    </li>)
+    <div key={el.id} style={{border: '1px solid #000', padding: "0.5rem", margin: "10px 10px"}}>
+        <h2 style={{marginTop: 5}}>{el.message}</h2>
+        <span>Author: {el.author}</span>
+        <span style={{marginLeft: '80%'}}>{el.date}</span>
+    </div>
+  )
 
   return (
-    <div style={{ margin: '3rem' }}>
-      <ul>
-        {lists}
-      </ul>
-     </div>
-
-     
+    <div>
+      {lists}
+    </div>
   );
   }
 
